30ca970ea747947be9c208bdb6eb063825842f2d
dogetest <32025243+dogetest@users.noreply.github.com>
2017-09-17 01:12:36 -0300

    Add files via upload

--- init.lua
+++ init.lua
@@ -1,68 +1,29 @@
------------------------------------------------------------------------------
--- JSONRPC4MinetestLua: JSONRPC4Lua modified for minetest engine.
--- json-rpc Module.
--- This module is released under the MIT License (MIT).
---
--- Requires JSON4MinetestLua to be installed.
--- 
--- USAGE:
--- This module exposes two functions:
---   rpc.proxy( 'url', callback)
---     Returns a proxy object for calling the 
---     JSON RPC Service at the given url with
---     given callback.
---   rpc.call ( 'url', 'method', callback, ...)
---     Calls the JSON RPC server at the given url, 
---     invokes the appropriate method, and passes the
---     remaining parameters. Minetest HTTPApiTable is
---     used to fetch pages and calls a callback.
---     Callback function receives result from 
---     decoded httpResponse.data as callback(res)
+dogecoin={}
 
-rpc = {}
-local http = minetest.request_http_api()
+local rpcport, rpcaddress="44555","127.0.0.1"
 
-function rpc.call(url, method, callback, ...)
-  local jsonRequest = json.encode(
-    {
-      ["id"]=tostring(math.random()),
-      ["method"] = method,
-      ["params"] = {...},
-    }
-  )
-  --debug
-  --minetest.debug("jsonRequest: "..jsonRequest)
-  http.fetch(
-    {
-      ['url'] = url,
-      ['method'] = 'POST',
-      ['extra_headers'] = { 
-        ['content-type']='application/json-rpc', 
-        ['content-length']=string.len(jsonRequest)},
-      ['post_data'] = jsonRequest
-    },
-    function(httpResponse)
-      if httpResponse.code ~= 200 then
-        return
-      end
-      if not callback then
-        return
-      end
-      local data = json.decode(httpResponse.data)
-      local res = data.result
-      callback(res)
-    end)
+if minetest.setting_get("doge.rpcaddress") then
+  rpcaddress=minetest.setting_get("doge.rpcaddress")
+end 
+
+if minetest.setting_get("doge.rpcport") then
+ rpcport=minetest.setting_get("doge.rpcport")
+end 
+
+dogecoin.url='http://'..
+  minetest.setting_get("doge.rpcuser")..
+  ':'..minetest.setting_get("doge.rpcpassword")..
+  '@'..rpcaddress..
+  ':'..rpcport
+
+local send=function(from, to, amount, reason)
+  local proxy=rpc.proxy(dogecoin.url)
+  proxy.sendmany(from, {[to]=amount, }, 1, reason)
 end
 
-function rpc.proxy(url, callback)
-  local serverProxy = {}
-  local proxyMeta = {
-    __index = function(self, key)
-      return function(...)
-        return rpc.call(url, key, callback, ...)
-      end
-    end
-  }
-  setmetatable(serverProxy, proxyMeta)
-  return serverProxy
+dogecoin.transfer=function(from, to, amount, reason)
+  local proxy=rpc.proxy(dogecoin.url, function(res)
+    send(from, res, amount, reason)
+    end)
+  proxy.getnewaddress(to)
 end
